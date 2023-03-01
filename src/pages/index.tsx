4c20fe34deaa4f7f44cb08abec9f747bf5b9b5ff
MatheusMena <matheusmena1@hotmail.com>
2023-03-01 02:08:01 -0300

    commit fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <h1 className='bg-slate-600'>OI</h1>
     </>
   )
 }
