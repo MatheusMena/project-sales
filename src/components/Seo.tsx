7f4c635b408af948bced0cbb76116e8cd14e1129
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-03-01 02:05:43 -0300

    💡
      Add Component "Seo" na qual podemos usar o Head de forma Clean

---
+++ src/components/Seo.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import { ISeo } from '../Types/iSeo'
+
+export const Seo = ({ title, description }: ISeo) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name='description' content={description && description} />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+  )
+}
