6ce82de195623e07d35263360783436fa2958f79
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-03-05 13:08:52 -0300

    update format

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@/components/HomePages/Header'
 import { Banner } from '../components/HomePages/Banner'
-export default function HomePage () {
+export const HomePage = () => {
   return (
     <>
-     <Header /> 
+      <Header />
       <Banner />
     </>
   )
 }
+
+export default HomePage
