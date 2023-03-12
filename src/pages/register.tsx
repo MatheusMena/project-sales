94241a64003d292aa9fb99b7635e77e5b2dd13bb
MatheusMena <matheusmena1@hotmail.com>
2023-03-12 19:12:26 -0300

    Create link to LoginPage

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,10 +1,21 @@
+import Link from "next/link";
+
 export default function register() {
     return (
         <div className="flex items-center justify-center min-h-screen lg:px-20 text-center bg-slate-300">
-            <div></div>
+            
             <div className="bg-slate-100 lg:static absolute z-10 rounded-r-lg shadow-2xl flex flex-col items-center justify-center w-screen h-screen sm:w-1/2 sm:h-[50%] sm:py-10 lg:h-[80vh] lg:px-20 ">
                 <h1 className="py-10 text-5xl font-sans">Criar conta</h1>
                 <div className="bg-slate-300 rounded-md  p-3 my-1 flex items-center">
+                    
+                    <input
+                        className=" bg-slate-300 rounded-md w-56 outline-none placeholder:italic"
+                        type="text"
+                        placeholder="Nome"
+                    />
+                </div>
+                <div className="bg-slate-300 rounded-md  p-3 my-1 flex items-center">
+
                     <input
                         className=" bg-slate-300 rounded-md w-56 outline-none placeholder:italic"
                         type="email"
@@ -22,8 +33,10 @@ export default function register() {
                     <button className="bg-green-500 m-1 p-2 rounded-lg hover:bg-green-600">
                         Criar Conta
                     </button>
-
                 </div>
+                <Link href="/LoginPage" className=''>
+              ja possui uma conta?
+            </Link>
             </div>
         </div>
     );
