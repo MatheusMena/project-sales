2c874bfc08a1e14c30761b0eda0603024e371739
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-03-17 09:46:57 -0300

    🧪

--- src/components/Common/CardPost.tsx
+++ src/components/Common/CardPost.tsx
@@ -1,5 +1,5 @@
-import { IProjectPost } from '@/Types/IProjectPost'
 import Image from 'next/image'
+import { useState } from 'react'
 export const CardPost = ({
   url_project,
   name_project,
@@ -7,11 +7,20 @@ export const CardPost = ({
   description,
   grup
 }: IProjectPost) => {
+  const [isActiveInfo, setIsActiveInfo] = useState(true)
+
+  const acttiveProject = () => {
+    setIsActiveInfo(!isActiveInfo)
+  }
+
   return (
-    <div className='w-auto md:w-60  flex flex-col overflow-hidden relative'>
+    <div
+      onClick={acttiveProject}
+      className='w-auto md:w-60  h-80 md:m-10 m-5 flex rounded-lg flex-col overflow-hidden'
+    >
       <div>
         <Image
-          className='w-full h-96 bg-cover object-cover'
+          className='w-full h-80 bg-cover object-cover'
           src={url_project}
           alt='foto de projeto'
           width={500}
@@ -19,35 +28,45 @@ export const CardPost = ({
           unoptimized
         />
       </div>
-      <div className='absolute top-0'>
-        <div className='w-60 flex flex-col gap-2  bg-gradient-to-t from-gray-900 via-gray-700  '>
-          <h3>{name_project}</h3>
+      <div
+        aria-checked={isActiveInfo}
+        className='
+        aria-checked:-top-full 
+        aria-checked:duration-700
+       h-full flex flex-col md:justify-end justify-center items-center gap-3  m-auto relative -top-[38%] md:hover:-top-full transition-all duration-500 md:hover:duration-700 px-5 bg-gradient-to-t from-gray-900 via-gray-700  '
+      >
+        <div className='w-60 flex flex-col gap-2'>
+          <h3 className='text-xl font-medium text-orbe bg-white py-1 pl-2 rounded-bl-xl rounded-tl-sm'>
+            {name_project}
+          </h3>
           <ul className='flex overflow-hidden gap-2.5'>
             {tags.map(tag => (
               <li>{tag}</li>
             ))}
           </ul>
           <span className='flex gap-5'>
-            <button>Salvar</button>
-            <button>Adquirir</button>
+            <button className=' font-medium p-1 border-2 border-spririt rounded-md hover:bg-spririt active:bg-kimono active:text-spririt active:border-transparent transition-all duration-75'>
+              Salvar
+            </button>
+            <button className=' font-medium p-1 border-2 border-spririt rounded-md hover:bg-spririt active:bg-kimono active:text-spririt active:border-transparent transition-all duration-75'>
+              Adquirir
+            </button>
           </span>
         </div>
-        <div className='flex flex-col gap-4 my-2 mx-1'>
+        <div className='md:w-52 h-1/2 flex flex-col gap-4 my-2 md:mr-10 mx-1 overflow-scroll'>
           <p>{description}</p>
-          <div>
-            <div className='flex flex-row overflow-hidden'>
-              {grup.map(grup => (
-                <Image
-                  src={grup.url_img}
-                  alt='foto de perfil'
-                  width={40}
-                  height={40}
-                  unoptimized
-                  className='rounded-full'
-                />
-              ))}
-            </div>
-          </div>
+        </div>
+        <div className='h-14 mr-8 mb-2 flex flex-row gap-3 overflow-x-scroll'>
+          {grup.map(grup => (
+            <Image
+              src={grup.url_img}
+              alt='foto de perfil'
+              width={40}
+              height={40}
+              unoptimized
+              className='rounded-full mb-2'
+            />
+          ))}
         </div>
       </div>
     </div>
